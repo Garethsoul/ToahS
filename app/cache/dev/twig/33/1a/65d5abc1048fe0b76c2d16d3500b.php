b979464a7d2d5515381390b34ead8548d74f708e
Erick Blangino <blanginoe@gmail.com>
2013-04-14 15:02:54 -0300

    Comenzando

---
+++ app/cache/dev/twig/33/1a/65d5abc1048fe0b76c2d16d3500b.php
@@ -0,0 +1,32 @@
+<?php
+
+/* HomeBundle:Default:index.html.twig */
+class __TwigTemplate_331a65d5abc1048fe0b76c2d16d3500b extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "Hola mundo
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "HomeBundle:Default:index.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
---
+++ src/Toah/PlataformaWeb/HomeBundle/HomeBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Toah\PlataformaWeb\HomeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class HomeBundle extends Bundle
+{
+}
